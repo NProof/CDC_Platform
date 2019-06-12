a4cab5097d383fc783f75a046eed1e802f8f2096
Instant Contiki <user@instant-contiki.(none)>
2019-06-12 20:25:50 +0200

    style=

--- header/Board.h
+++ header/Board.h
@@ -8,6 +8,8 @@ struct Chess{
     uint8_t stat:2; // 0:unknown, 1:dark, 2:alive, 3:out_board 
     uint8_t type:4; // index of "XkgmrncpPCNRMGK-", lower case is black chess, upper case is red.
                     // (p > k) & (k > g > m > r > n) & (g > m > r > n > p)
+                    //  k 將 g 士 m 象 r 車  n 馬 c 包 p 卒 K 帥 G 仕 M 相 R ? N 嗎 C 炮 P 兵
+                    //   1    2    3    4     5     6    7   14   13   12   11  10   9    8
     uint8_t :0;
 
     bool operator==(const Chess &) const;
@@ -42,17 +44,18 @@ class Board{
 bool neighbor(int, int);
 
 /*
-bool boom(int x,int y,Board chess[]){ // 這裡你把 int chess[] 改成 Board & board
+bool boom(int x,int y,Board board){ // 這裡你把 int chess[] 改成 Board & board
     int tem;
+
     if(y>x){
         tem = y;
         y = x;
         x = tem; 
     }
-    if(x-y==2&&x%8!=0&&(x-1)%8!=0&&chess[x-1]==1){
+    if(x-y==2&&x%8!=0&&(x-1)%8!=0&&board.chs[x-1].stat!=3){
         return true;
     }
-    else if(x-y==16){
+    else if(x-y==16&&board.chs[x-8].stat!=3){
         return true;
     }else return false;
 }*/
