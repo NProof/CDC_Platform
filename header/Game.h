c944ecffe99ffef80020cfd13dcff7739d0e0750
NProof <endles00000@gmail.com>
2019-06-30 19:38:44 +0200

    reduce game's ctr() and add class Player

--- header/Game.h
+++ header/Game.h
@@ -1,22 +1,30 @@
 #ifndef GAME_H_
 #define GAME_H_
 
-#include "../header/Board.h"
+#include "Board.h"
+#include "Player.h"
 #include <string>
+
+class Player;
 
 struct Ins{
 
 };
 
+struct StateG{
+    int col;
+};
+
 class Game{
 public:
     Game();
-    Game(std::string);
-    Game(std::string, uint32_t, uint32_t);
+//    Game(std::string);
+    Game(std::string, uint32_t = 0xffffffff, uint32_t = 0xffffffff);
     ~Game();
     Game(const Game &) = delete;
   //makeIns(Ins &);
 //pr1ivate:
+    Player * players[3];
     int turn;
     int col[3] = {0, 0};
     Board * board;
--- src/Game.cc
+++ src/Game.cc
@@ -12,18 +12,6 @@ Game::Game() : turn(0) {
     }
 }
 
-Game::Game(std::string str) : turn(0) {
-    std::map<char, uint8_t> m = {
-        {'X',0}, {'k',1}, {'g',2}, {'m',3}, {'r',4}, {'n',5}, {'c',6}, {'p',7}, 
-        {'P',8}, {'C',9}, {'N',10}, {'R',11}, {'M',12}, {'G',13}, {'K',14}, {'-',15}
-    };
-    board = new Board();
-    for(int i = 0; i < 32; i++){
-        Chess * ch = new Chess(m.at(str[i]), 1);
-        board->pickDw(i, ch);
-    }
-}
-
 Game::Game(std::string str, uint32_t alive, uint32_t dark) : turn(0) {
     std::map<char, uint8_t> m = {
         {'X',0}, {'k',1}, {'g',2}, {'m',3}, {'r',4}, {'n',5}, {'c',6}, {'p',7},
